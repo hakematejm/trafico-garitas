64ac49a0efe6b866a07fe808098abc7d1551a5c1
hakem <hakematejm@gmail.com>
2025-06-03 17:28:38 -0700

    vercel.json en la raíz del proyecto

--- api/garita.js
+++ api/garita.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ mensaje: "La API garita está funcionando correctamente ✅" });
+  res.status(200).json({ mensaje: "¡API funcionando!" });
 }
